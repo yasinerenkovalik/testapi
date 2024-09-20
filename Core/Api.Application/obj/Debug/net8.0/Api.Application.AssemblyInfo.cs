ddc4a166b330f9f4b8740bc17d8e057074269914
Yasin Eren Kovalık <yasinerenkovalik@gmail.com>
2024-09-20 12:07:00 +0300

    20.09.2024

--- Core/Api.Application/obj/Debug/net8.0/Api.Application.AssemblyInfo.cs
+++ Core/Api.Application/obj/Debug/net8.0/Api.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f647101d17a2d89f890efbbf8ec7aa9d5fd75d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
